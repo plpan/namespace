4f1de8d249a08f2f472848d4e942d677d66262f3
plpan <pplonepiece@126.com>
2017-12-20 18:35:47 +0000

    ipc

---
+++ ipc/ipc.c
@@ -0,0 +1,55 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+// global status
+int checkpoint[2];
+
+static char child_stack[STACK_SIZE];
+
+char* const child_args[] = {
+    "/bin/bash",
+    NULL
+};
+
+// 1. 在parent中关闭写
+// 2. 在child中等待EOF
+
+int child_main(void* arg) {
+	// [child] wait
+	char c; // stub char
+	// [child] init sync primitive
+	close(checkpoint[1]);
+	// wait...
+	read(checkpoint[0], &c, 1);
+
+    printf(" - World !\n");
+	sethostname("stupig", 6);
+    execv(child_args[0], child_args);
+    printf("Ooops\n");
+    return 1;
+}
+
+int main() {
+	// [parent] init
+	pipe(checkpoint);
+
+    printf(" - Hello ?\n");
+    int child_pid = clone(child_main, child_stack+STACK_SIZE,
+        CLONE_NEWUTS | CLONE_NEWIPC | SIGCHLD, NULL);
+
+	// long init job
+	sleep(4);
+	// signal 'done'
+	close(checkpoint[1]);
+
+    waitpid(child_pid, NULL, 0);
+    return 0;
+}
+
